b8b3d40005f4478d4453e3f66e39e00c529180bc
codeamac02 <codeamac02@gmail.com>
2016-05-11 13:54:52 -0500

    Crea el método global_linear_search

--- linearsearch.rb
+++ linearsearch.rb
@@ -5,11 +5,27 @@ def linear_search(num, array)
       return i
     end
     i += 1
-      end
-      end
+  end
+end
 
   random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
 p linear_search(20, random_numbers)
 # => 3
 p linear_search(29, random_numbers)
 # => nil
+
+def global_linear_search(valor, arr)
+  indices = []
+  i = 0
+  for i in 0..arr.length
+    if arr[i] == valor
+      indices << i
+    end
+    i += 1
+  end  
+  indices     
+end
+  arr = "entretenerse".split('')
+
+p global_linear_search("e", arr)
+# => [0, 4, 6, 8, 11]
